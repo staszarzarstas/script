84ee04406dba009bcb6329c6669eff6583ea9b1f
Stanislav <royultra77@gmail.com>
2025-03-20 18:05:51 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/script",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
